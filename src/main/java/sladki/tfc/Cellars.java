4be413af447a6907830c2696fc82fe1f57c8decb
Pavel Vasilev <explosere@ya.ru>
2015-12-08 17:28:48 +0300

    Initial commit (transition to gradle)

---
+++ src/main/java/sladki/tfc/Cellars.java
@@ -0,0 +1,48 @@
+package sladki.tfc;
+
+import net.minecraftforge.common.MinecraftForge;
+import sladki.tfc.Handlers.ChunkEventHandler;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(name = Cellars.MODNAME, modid = Cellars.MODID, version = Cellars.VERSION, dependencies = "after:terrafirmacraft")
+public class Cellars {
+	
+	public static final String MODID = "tfccellars";
+	public static final String MODNAME = "CellarsAddon";
+	public static final String VERSION = "1.010";
+	
+	@Instance("tfccellars")
+    public static Cellars instance;
+	
+	@SidedProxy(clientSide = "sladki.tfc.ClientProxy", serverSide = "sladki.tfc.CommonProxy")
+	public static CommonProxy proxy;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		ModConfig.loadConfig(event);
+	}
+	
+	@EventHandler
+	public void initialize(FMLInitializationEvent event) {
+		MinecraftForge.EVENT_BUS.register(new ChunkEventHandler());
+		
+		ModManager.loadBlocks();
+		ModManager.registerBlocks();
+				
+		ModManager.loadItems();
+		ModManager.registerItems();
+		
+		ModManager.registerTileEntities();
+		ModManager.registerRecipes();
+		
+		proxy.registerRenderInformation();
+		proxy.registerGuiHandler();
+		proxy.hideItemsNEI();
+	}
+	
+}
