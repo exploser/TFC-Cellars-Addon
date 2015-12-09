36cc0aebfcd10cdebe9314ed9292c9c24bf0a555
Pavel Vasilev <explosere@ya.ru>
2015-12-09 16:00:37 +0300

    attempt to synt temp and coolant with client... still doesn't work really

--- src/main/java/sladki/tfc/Gui/GuiIceBunker.java
+++ src/main/java/sladki/tfc/Gui/GuiIceBunker.java
@@ -16,13 +16,11 @@ public class GuiIceBunker extends GuiContainer {
 
 	private static ResourceLocation texture = new ResourceLocation(Cellars.MODID, "textures/gui/gui_iceBunker.png");
 	private static Minecraft mc = Minecraft.getMinecraft();
-	private int coolantAmount = 0;
-	private float temperature = 0;
+	TEIceBunker teBunker;
 
 	public GuiIceBunker(InventoryPlayer inventoryPlayer, TEIceBunker tileEntity) {
 		super(new ContainerIceBunker(inventoryPlayer, tileEntity));
-		coolantAmount = tileEntity.getCoolantAmount();
-		temperature = tileEntity.getTemperature();
+		teBunker = tileEntity;
 	}
 
 	@Override
@@ -37,13 +35,26 @@ public class GuiIceBunker extends GuiContainer {
 
 	@Override
 	protected void drawGuiContainerForegroundLayer(int param1, int param2) {
+		teBunker.updateEntity();
+		int coolantAmount = teBunker.getCoolantAmount();
+		float temperature = teBunker.getTemperature();
+		
 		// draw text and stuff here
 		// the parameters for drawString are: string, x, y, color
 		fontRendererObj.drawString("Coolant:", 8, 32, 0);
-		fontRendererObj.drawString(coolantAmount + "/1200", 8, 42, 0);
+		// fontRendererObj.drawString(coolantAmount + "/120", 8, 42, 0);
 		fontRendererObj.drawString("Temp.:", xSize - 50, 32, 0);
 		fontRendererObj.drawString(Math.round(temperature) + "", xSize - 50, 42, 0);
+		// draw frame
+		drawRect(7, 41, 7 + 50, 41 + 8, 0xFF000000);
+		// draw coolant indicator
+		int length = 0;
+		if (coolantAmount > 0)
+			length = 48 * coolantAmount / 120;
+		
+		this.drawGradientRect(8, 42, 8 + length, 42 + 6, 0xFF890020, 0xFF000089);
 		// draws "Inventory" or your regional equivalent
 		mc.fontRenderer.drawString(StatCollector.translateToLocal("container.inventory"), 8, ySize - 96 + 2, 0);
 	}
+
 }
